f384a08b2d724484608e403c47a528980e56d1ed
Kevin Yugla <kiyugla@espe.edu.ec>
2025-07-15 21:49:28 -0500

    mejoré el mensaje inicial: agregué instrucciones más claras para el usuario y preparé la interfaz para recibir datos

--- public/COMPONENTS/par-impar-listar.js
+++ public/COMPONENTS/par-impar-listar.js
@@ -2,7 +2,7 @@ class ComponenteListar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div id="areaResultados" class="text-center text-light">
-        <p class="mb-0">Esperando datos...</p>
+        <p class="mb-0">Ingrese un rango de números para ver si son pares o impares</p>
       </div>
     `;
   }
